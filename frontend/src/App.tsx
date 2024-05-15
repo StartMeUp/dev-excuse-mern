4fedb2ea1479ebb6dd20fb921b1196bd9bb72cf7
StartMeUp <contact@start-me-up.fr>
2024-05-15 09:59:52 +0200

    merge install-frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1 className="text-red-500 font-bold">Hello, World!</h1>;
+};
+
+export default App;
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      animation: {
+        fadeIn2s: "fadeIn 1.5s linear",
+        fadeIn300ms: "fadeIn .3s linear",
+        fadeOut300ms: "fadeOut .3s linear",
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: "0" },
+          to: { opacity: "1" },
+        },
+        fadeOut: {
+          from: { opacity: "1" },
+          to: { opacity: "0" },
+        },
+      }
+    },
+  },
+  plugins: [],
+};
