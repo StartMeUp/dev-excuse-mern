905be87805ed076484fb46bfab4f1ca7c22ab254
StartMeUp <contact@start-me-up.fr>
2024-05-15 21:35:49 +0200

    add tanstack hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import { useGetAllExcuses } from "./hooks/tanstack.hooks";
+
 const App = () => {
-  return <h1 className="text-red-500 font-bold">Hello, World!</h1>;
+  const { data: excuses, isLoading, isError } = useGetAllExcuses();
+
+  if (isLoading) return <h1>Loading...</h1>;
+
+  if (isError) return <h1>Error!</h1>;
+
+  if (!excuses) return <h1>No excuses found</h1>;
+
+  return (
+    <ul>
+      {excuses.data.map((excuse) => (
+        <li key={excuse._id}>{excuse.message}</li>
+      ))}
+    </ul>
+  );
 };
 
 export default App;
