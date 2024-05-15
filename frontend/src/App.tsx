f35a7919f21cccce7087440bb4f749cbabe1e722
StartMeUp <contact@start-me-up.fr>
2024-05-15 09:54:42 +0200

    app ready for dev

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-  return <h1 className="text-red-500 font-bold">Hello, Worldddd!</h1>;
+  return <h1 className="text-red-500 font-bold">Hello, World!</h1>;
 };
 
 export default App;
